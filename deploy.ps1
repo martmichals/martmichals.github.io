19398d68bb9b953657edb2e57768363fae495e2f
Martin Michalski <martmichals@gmail.com>
2021-11-12 00:26:31 -0600

    Added deployment script

---
+++ deploy.ps1
@@ -0,0 +1,20 @@
+# Build locally
+git checkout master
+# jekyll build
+
+# Create temporary folder
+$T="$($Env:temp)\tmp$([convert]::tostring((get-random 65535),16).padleft(4,'0')).tmp"
+New-Item -ItemType Directory -Path $T
+
+# Copy HTML source into the temporary folder along with gitignore
+Copy-Item .\_site\* -Destination $T -Recurse
+Copy-Item .gitignore -Destination $T
+
+# Switch branches to the live branch, push changes
+git checkout gh-pages
+git add .
+git commit -m "Deployment of portfolio"
+git push
+
+# Change back to dev branch
+git checkout master
