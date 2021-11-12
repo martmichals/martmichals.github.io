1b952a03457408d654570d1a2b1647340b5d1ef7
Martin Michalski <martmichals@gmail.com>
2021-11-12 01:16:34 -0600

    Mini deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,35 @@
+function Invoke-Utility {
+
+<#
+.SYNOPSIS
+Invokes an external utility, ensuring successful execution.
+
+.DESCRIPTION
+Invokes an external utility (program) and, if the utility indicates failure by 
+way of a nonzero exit code, throws a script-terminating error.
+
+* Pass the command the way you would execute the command directly.
+* Do NOT use & as the first argument if the executable name is not a literal.
+
+.EXAMPLE
+Invoke-Utility git push
+
+Executes `git push` and throws a script-terminating error if the exit code
+is nonzero.
+#>
+
+  $exe, $argsForExe = $Args
+  # Workaround: Prevents 2> redirections applied to calls to this function
+  #             from accidentally triggering a terminating error.
+  #             See bug report at https://github.com/PowerShell/PowerShell/issues/4002
+  $ErrorActionPreference = 'Continue'
+  try { & $exe $argsForExe } catch { Throw } # catch is triggered ONLY if $exe can't be found, never for errors reported by $exe itself
+  if ($LASTEXITCODE) { Throw "$exe indicated failure (exit code $LASTEXITCODE; full command: $Args)." }
+}
+
 # Build locally
-git checkout master
-# jekyll build
+Invoke-Utility git checkout master
+jekyll build
 
 # Create temporary folder
 $T="$($Env:temp)\tmp$([convert]::tostring((get-random 65535),16).padleft(4,'0')).tmp"
@@ -11,10 +40,14 @@ Copy-Item .\_site\* -Destination $T -Recurse
 Copy-Item .gitignore -Destination $T
 
 # Switch branches to the live branch, push changes
-git checkout gh-pages
-git add .
-git commit -m "Deployment of portfolio"
-git push
+Invoke-Utility git checkout gh-pages
 
-# Change back to dev branch
-git checkout master
+# Add the changes
+Remove-Item .\* -Recurse
+Copy-Item $T\* -Destination . -Recurse
+# git add .
+# git commit -m "Deployment of portfolio"
+# git push
+
+# # Change back to dev branch
+# git checkout master
